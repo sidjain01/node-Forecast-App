a416f99c03658c5c7a8582a59656076f60889db2
Siddhant <sidsanjeevjain@gmail.com>
2020-03-21 17:00:24 +0530

    editing environment variables

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
+const port = process.env.PORT || 3000
 
 //Defining path for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -92,6 +93,6 @@ app.get('*',(req,res) =>{
     })
 })
 
-app.listen(3000,() =>{
-    console.log('The server is up on port 3000')
+app.listen(port ,() =>{
+    console.log('The server is up on port '+ port)
 })
